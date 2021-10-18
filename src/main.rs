93a0e8cbb7409b5dccae0d2b7cfc9fb9b47142c3
Panadero1 <jacob.anderson.wa.us@gmail.com>
2021-10-18 16:55:50 -0700

    todo

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 // [TODO]
-// Get world generating
-// Region drawing
+// Proper loading/creation logic
+// Think up world regions
+// Tile interactions
+// Resolution saving
 
 // [Housekeeping]
 // Fix options page
 
 // [Backlog]
 // Figure out an answer to MM's questions
-// Persist to file
-// Think up world regions (story)
+// New list of sprites
+// Hat properties
 
 fn main() {
     sonar::run();
