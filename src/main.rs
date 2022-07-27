0c75372d7a53faa0b0ca660b18c680ec6dba4655
Panadero1 <jacob.anderson.wa.us@gmail.com>
2022-07-26 21:26:11 -0700

    todo rearrange

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,6 @@
 //! wonderfill is a game about exploring a world and enjoying your time
 //! that's about it! :))
 
-// [Emergency]
-
-// [Todo]
-// Make a build script that copies the image assets
-// Make entities
-// Color config file
-// Finish starting tutorial area
-// When minigame over, return PostOperation
-
-// [Maybe]
-// Consolidate drawing requirements into one struct
-// Save compression
-
-// [Housekeeping]
-// Fix options page
-
-// [Backlog]
-// New list of sprites
-// Hat properties
-// Comment your code lol
-// Add music
-// Asset editor message tells where to color config
-// Make options work + config file + add to build script
-
-// [Before release]
-// Remove invisible wall texture
-
 fn main() {
     wonderfill::run();
 }
